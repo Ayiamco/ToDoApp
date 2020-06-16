beaf3391a0795621d2624f4269d3ad16a1ae5098
joseph achonu <josephachonu@gmail.com>
2020-06-16 22:39:55 +0100

    feature: Added delete single task button

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-//get task from local storage
+//get task from local storage to GWHDKJFHSKJDHJ ghkwghkwegh kjhdkhj kjhkdK
 window.addEventListener("load",fillData())
 
 function fillData(){
-    
     try{
-        console.log(localStorage)
-        let taskArray=localStorage["tasks"].split('||')
-        console.log(taskArray)
-        for (let task of taskArray){
-            let myLi=document.createElement('li')
-            myLi.textContent=task
-            document.querySelector('ul').appendChild(myLi)
-        }
-        document.querySelector('h3').style.display='block'
-        document.querySelector('ul').style.display='block'
-        document.querySelector('.removeAllTask').style.display='block'
+        localStorage["tasks"].split('||')
+        if (!String(localStorage.length)==0){
+            console.log(String(localStorage["tasks"]))
+            document.querySelector('ul').innerHTML=localStorage["tasks"]
+            document.querySelector('h3').style.display='block'
+            document.querySelector('ul').style.display='block'
+            document.querySelector('.removeAllTask').style.display='block'
+        }  
     }
     catch(err){
-        console.log("No previous task added")
+        
+        console.log("New User")
     }
-        
-        
-        
-        }
+         
+}
 
 
 
@@ -32,7 +27,8 @@ document.querySelector('.addTask').addEventListener('click',()=>{
     let myLi=document.createElement('li')
     document.querySelector('.input').value=''
     if (!task==''){
-    myLi.textContent=task
+    let taskHtml=`<li class='task'>${task}.<br><button class="remove" onclick="removeItem(this)"> Remove</button></li>`
+    myLi.innerHTML=taskHtml
     document.querySelector('ul').appendChild(myLi)
     document.querySelector('.tooltip').style.display='none'
     document.querySelector('h3').style.display='block'
@@ -40,17 +36,18 @@ document.querySelector('.addTask').addEventListener('click',()=>{
     document.querySelector('.removeAllTask').style.display='block'
     
         if(localStorage["tasks"]==null){
-            localStorage["tasks"]=task
-            console.log("added frist item")
+            localStorage["tasks"]=taskHtml
+            console.log("added first item")
             console.log(localStorage["tasks"])
         }
         else{
-            localStorage["tasks"]=localStorage["tasks"] + "||" +task
+            localStorage["tasks"]=localStorage["tasks"]  +taskHtml
+            console.log("Added new task")
         }
 
     }
     else{
-        console.log('here')
+        console.log('Empty Entry')
         document.querySelector('.tooltip').style.display='block'
     }
 })
@@ -63,3 +60,26 @@ document.querySelector('.removeAllTask').addEventListener('click',() =>{
     localStorage.removeItem('tasks')
 
 })
+
+function removeItem(node){
+    
+    let taskHtml= localStorage["tasks"]
+    let taskList= document.createElement('ul')
+    taskList.innerHTML=taskHtml
+    childs = taskList.childNodes
+    let currentTask=(node.parentElement)
+    let currentTaskIndex=1
+    for(let index=0;index<childs.length;index++){
+        if (currentTask.textContent==childs[index].textContent){
+            console.log('here: ',childs[index])
+            childs[index].remove()
+            currentTaskIndex=index
+        }
+        
+    }
+    
+    document.getElementsByClassName('task').item(currentTaskIndex).remove()
+
+    localStorage['tasks']=document.querySelector('ul').innerHTML
+}
+
